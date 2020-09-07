d63bd4a26adbde1900b01d078cb688235ef1170d
Türker Tunalı <turkert@hotmail.com>
2020-09-07 11:41:02 +0300

    Add Turkish translation for datepicker
    Added Turkish translation for datepicker.

--- frappe/public/js/lib/datepicker/locale-all.js
+++ frappe/public/js/lib/datepicker/locale-all.js
@@ -89,7 +89,6 @@
     firstDay: 0
 }; })(jQuery);
 
-
 ;(function ($) { $.fn.datepicker.language['es'] = {
     days: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
     daysShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
@@ -248,6 +247,33 @@
     timeFormat: 'hh:ii',
     firstDay: 1
 }; })(jQuery);
+
+;(function ($) { $.fn.datepicker.language['en-US'] = {
+    days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+    months: ['January','February','March','April','May','June', 'July','August','September','October','November','December'],
+    monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    today: 'Today',
+    clear: 'Clear',
+    dateFormat: 'mm/dd/yyyy',
+    timeFormat: 'hh:ii aa',
+    firstDay: 0
+}; })(jQuery);
+
+;(function ($) { $.fn.datepicker.language['tr'] = {
+    days: ['Pazar', 'Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi'],
+    daysShort: ['Pzr', 'Pzt', 'Sal', 'Çrş', 'Per', 'Cum', 'Cts'],
+    daysMin: ['Pz', 'Pt', 'Sa', 'Ça', 'Pe', 'Cu', 'Ct'],
+    months: ['Ocak','Şubat','Mart','Nisan','Mayıs','Haziran', 'Temmuz','Ağustos','Eylül','Ekim','Kasım','Aralık'],
+    monthsShort: ['Ock', 'Şbt', 'Mrt', 'Nis', 'May', 'Haz', 'Tem', 'Ağu', 'Eyl', 'Ekm', 'Kas', 'Ara'],
+    today: 'Bugün',
+    clear: 'Temizle',
+    dateFormat: 'dd/mm/yyyy',
+    timeFormat: 'hh:ii',
+    firstDay: 1
+}; })(jQuery);
+
 
 ;(function ($) { $.fn.datepicker.language['zh'] = {
     days: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
